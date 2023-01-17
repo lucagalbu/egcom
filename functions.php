c895cea672bc202c23305567b4e3ca4a4f417b08
lucagalbu <luca.galbusera@unibas.ch>
2023-01-17 20:35:42 +0100

    Added portfolio as post type

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * egcom functions and definitions
  *
@@ -7,9 +8,9 @@
  * @package egcom
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -19,17 +20,18 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function egcom_setup() {
+function egcom_setup()
+{
 	/*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
 		* If you're building a theme based on egcom, use a find and replace
 		* to change 'egcom' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'egcom', get_template_directory() . '/languages' );
+	load_theme_textdomain('egcom', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	add_theme_support('automatic-feed-links');
 
 	/*
 		* Let WordPress manage the document title.
@@ -37,19 +39,19 @@ function egcom_setup() {
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
-	add_theme_support( 'title-tag' );
+	add_theme_support('title-tag');
 
 	/*
 		* Enable support for Post Thumbnails on posts and pages.
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'egcom' ),
+			'menu-1' => esc_html__('Primary', 'egcom'),
 		)
 	);
 
@@ -83,7 +85,7 @@ function egcom_setup() {
 	);
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	add_theme_support('customize-selective-refresh-widgets');
 
 	/**
 	 * Add support for core custom logo.
@@ -100,7 +102,7 @@ function egcom_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'egcom_setup' );
+add_action('after_setup_theme', 'egcom_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -109,22 +111,24 @@ add_action( 'after_setup_theme', 'egcom_setup' );
  *
  * @global int $content_width
  */
-function egcom_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'egcom_content_width', 640 );
+function egcom_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('egcom_content_width', 640);
 }
-add_action( 'after_setup_theme', 'egcom_content_width', 0 );
+add_action('after_setup_theme', 'egcom_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function egcom_widgets_init() {
+function egcom_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'egcom' ),
+			'name'          => esc_html__('Sidebar', 'egcom'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'egcom' ),
+			'description'   => esc_html__('Add widgets here.', 'egcom'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -132,22 +136,23 @@ function egcom_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'egcom_widgets_init' );
+add_action('widgets_init', 'egcom_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function egcom_scripts() {
-	wp_enqueue_style( 'egcom-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'egcom-style', 'rtl', 'replace' );
+function egcom_scripts()
+{
+	wp_enqueue_style('egcom-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_style_add_data('egcom-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'egcom-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('egcom-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'egcom_scripts' );
+add_action('wp_enqueue_scripts', 'egcom_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -172,7 +177,29 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Register the portfolio post type
+ */
+function create_portfoliotype()
+{
+	register_post_type(
+		'portfolio',
+		array(
+			'labels' => array(
+				'name' => __('Portfolio'),
+				'singular_name' => __('Portfolio')
+			),
+			'supports' => array('title', 'editor', 'excerpt', 'thumbnail'),
+			'public' => true,
+			'has_archive' => true,
+			'rewrite' => array('slug' => 'portfolio'),
+			'show_in_rest' => true,
+			'taxonomies' => array('category'),
+		)
+	);
+}
+add_action('init', 'create_portfoliotype');
