3b1113a0943b15ba49af51168c7a74257dd1f5e9
lucagalbu <luca.galbusera@unibas.ch>
2023-01-21 15:43:05 +0100

    Added featured portfolios on the homepage
    The portfolios shown are the ones that have the meta data "shown on the homepage"
    selected. This is added as a custo field using the plugin Advanced Custom Fields.

--- functions.php
+++ functions.php
@@ -112,6 +112,7 @@ function add_scripts_and_styles()
 {
 	// Custom styles
 	wp_enqueue_style('portfolio', get_template_directory_uri() . '/custom_assets/css/portfolio.css');
+	wp_enqueue_style('home_page', get_template_directory_uri() . '/custom_assets/css/home_page.css');
 
 	// Activate Masonry
 	wp_enqueue_script('activate-masonry', get_template_directory_uri() . '/custom_assets/js/masonry.js', array('jquery', 'masonry'), null);
--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -6,9 +6,38 @@
  * @package egcom
  */
 
+
+// Retrieve all posts of type portoflio. The meta key
+// is created using the Advanced Custom Fields plugin
+$portfolios =
+    $posts = get_posts(array(
+        'numberposts' => -1,
+        'post_type' => 'portfolio',
+        'meta_key' => 'featured_portfolio',
+        'meta_value' => true
+    ));
 ?>
 
 <!-- Start the rendering of the template -->
-<div>
-
+<div class="home-page container">
+    <?php for ($index = 0; $index < count($portfolios); $index++) {
+        $portfolio = $portfolios[$index];
+    ?>
+        <div class="row align-items-center justify-content-center py-5 gx-5">
+            <?php $title_col_class = 'col-6 ' . ($index % 2 ? 'order-last' : ''); ?>
+            <div class="<?= $title_col_class ?>">
+                <?php $description_class = $index % 2 ? 'float-start' : 'float-end'; ?>
+                <div class="<?= $description_class ?>">
+                    <h1><?= get_the_title($portfolio) ?></h1>
+                    <p><?= get_the_excerpt($portfolio) ?></p>
+                    <a href=<?= get_post_permalink($portfolio) ?>>Continua a leggere</a>
+                </div>
+            </div>
+            <?php $image_col_class = 'col-6 ' ?>
+            <div class="<?= $image_col_class ?>">
+                <?php $featured_image_class = "featured-image " . ($index % 2 ? 'float-end' : 'float-start'); ?>
+                <img class="<?= $featured_image_class ?>" src=<?= get_the_post_thumbnail_url($portfolio) ?>> </img>
+            </div>
+        </div>
+    <?php } ?>
 </div>
