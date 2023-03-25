5a645afc1f36afcb9e8e9db3c12c3bd67ace3a3c
lucagalbu <luca.galbusera@unibas.ch>
2023-03-25 17:48:09 +0100

    Reshaped portfolios page
    I removed the masonry and I use the same style as for the home page.

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -1,19 +1,4 @@
 <?php
-/*
-Template Name: Portfolio Masonry
-*/
-
-
-/**
- * The portfolio template.
- *
- * This page displays a masonry with all the portfolio feature images
- *
- * @author Luca Galbusera
- *
- * @package egcom
- */
-
 
 /**
  * Return all the posts of type portfolio.
@@ -41,28 +26,23 @@ get_header();
 $portfolio_items = getPortfolioPosts();
 ?>
 
-<main id="primary" class="site-main">
-    <div class="container-fluid">
-        <div class="grid">
-            <h1>New theme</h1>
-            <!-- The following two containers are required by masonry to set a 
-                relative size for the articles -->
-            <div class="grid-sizer"></div>
-            <div class="gutter-sizer"></div>
+<main id="portfolio" class="portfolio-list">
+    <div class=" container">
+        <div class="row ">
             <?php
             while ($portfolio_items->have_posts()) {
                 $portfolio_items->the_post();
             ?>
-                <div class="grid-item">
-                    <div class="card mb-4 d-flex flex-column align-items-center">
-                        <div class="portfolio_thumbnail">
-                            <a href=<?= get_post_permalink(); ?> target="_self">
-                                <img src=<?= the_post_thumbnail_url(); ?>></img>
-                            </a>
+                <div class="col-4 mb-3 mt-3 d-flex justify-content-center">
+                    <a class="project-container position-relative" href='<?= get_permalink() ?>'>
+                        <?= get_the_post_thumbnail(null, "post-title"); ?>
+                        <div class="d-flex justify-content-center align-items-center text-center post-title">
+                            <?= get_the_title(); ?>
                         </div>
-                        <p class="text-center mb-0"> <?= get_the_title(); ?> </p>
-                        <p class="text-center fw-light fst-italic"> <?= get_the_category()[0]->name; ?></p>
-                    </div>
+                        <div class="post-description">
+                            <?= get_the_excerpt(); ?>
+                        </div>
+                    </a>
                 </div>
             <?php } ?>
         </div>
