aada96ede902b2084876f30d6c5399a3777c9394
lucagalbu <luca.galbusera@unibas.ch>
2023-04-07 10:34:46 +0200

    Added fa icons support

--- functions.php
+++ functions.php
@@ -120,6 +120,7 @@ function add_scripts_and_styles()
 	wp_enqueue_style('single_portfolio', get_template_directory_uri() . '/custom_assets/css/single_portfolio.css');
 	wp_enqueue_style('recensioni', get_template_directory_uri() . '/custom_assets/css/recensioni.css');
 
+	wp_enqueue_script('fa-icons', 'https://kit.fontawesome.com/8dc7859c9e.js');
 	wp_enqueue_script('recensioni', get_template_directory_uri() . '/custom_assets/js/recensioni.js');
 }
 add_action('wp_enqueue_scripts', 'add_scripts_and_styles');
