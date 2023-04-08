9fc5ade1d9015f247eb4af861c215fc2b7a8a41a
lucagalbu <luca.galbusera@unibas.ch>
2023-04-08 12:02:59 +0200

    Added contatti

---
+++ page-contatti.php
@@ -0,0 +1,17 @@
+<?php
+get_header();
+?>
+
+<div id="contacts">
+    <div class="outer-padding title-strip">
+        <h1>Contatti</h1>
+    </div>
+
+    <div class="contacts-container d-flex flex-column align-items-center justify-content-center">
+        <p><i class="fa fa-envelope-o me-1" aria-hidden="true"></i>xxxx.xxxx@mail.org</p>
+        <p><i class="fa fa-phone me-1" aria-hidden="true"></i>+00 000 12345678</p>
+    </div>
+</div>
+
+<?php
+get_footer();
