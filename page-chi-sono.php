e0413b4753bf540724ac947bddbb641475d0cb47
lucagalbu <luca.galbusera@unibas.ch>
2023-04-10 12:00:01 +0200

    Chi Sono is responsive

--- page-chi-sono.php
+++ page-chi-sono.php
@@ -10,7 +10,7 @@ get_header();
     </div>
 
     <div class="outer-padding d-flex justify-content-center mt-4">
-        <div class="section-title col-4">
+        <div class="section-title col-10 col-sm-6 col-md-4">
             <h3 class="text-center">
                 <?= get_the_title() ?>
             </h3>
@@ -19,11 +19,16 @@ get_header();
     </div>
 
     <div class="outer-padding container-flex">
+        <div class="row d-sm-none justify-content-center">
+            <div class="col-5">
+                <?= get_the_post_thumbnail(null, "full") ?>
+            </div>
+        </div>
         <div class="row justify-content-center gx-5 mt-3">
-            <div class="col-6">
+            <div class="col-sm-8 col-xl-6 text-description">
                 <?= get_the_content() ?>
             </div>
-            <div class="col-3">
+            <div class="col-sm-4 col-xl-3 d-none d-sm-inline">
                 <?= get_the_post_thumbnail(null, "full") ?>
             </div>
         </div>
