db1867a49fd10e0a15f8c1c37b09e317e5b4fd5b
lucagalbu <luca.galbusera@unibas.ch>
2023-04-16 14:22:25 +0200

    Page Servizi is now responsive

--- page-servizi.php
+++ page-servizi.php
@@ -77,7 +77,7 @@ get_header();
         <h1>Servizi</h1>
     </div>
 
-    <div class="outer-padding d-flex justify-content-center mt-4">
+    <div class="container-xl">
         <?php echo do_shortcode(
             "[title_section]
                 Negli anni ho creato una rete di professionisti che mi
@@ -86,96 +86,97 @@ get_header();
             [/title_section]"
         ); ?>
 
-    <div class="outer-padding container-flex">
-        <div class="row mt-3 gy-4 gx-5">
-            <div class="col-4">
-                <?= get_tile(
-                    "document-pen-sign-svgrepo-com",
-                    "Ufficio Stampa & Media Relations",
-                    "Ogni canale ha la sua forma di comunicazione. Che sia la carta stampata, il web,
+        <div class="outer-padding container-flex">
+            <div class="row mt-3 gy-4 gx-5">
+                <div class="col-12 col-sm-6 col-xl-4">
+                    <?= get_tile(
+                        "document-pen-sign-svgrepo-com",
+                        "Ufficio Stampa & Media Relations",
+                        "Ogni canale ha la sua forma di comunicazione. Che sia la carta stampata, il web,
                     la radio o la Tv è necessario sviluppare un'attività di ufficio stampa ad hoc senza
                     dimenticare l'importanza di coltivare i rapporti con i giornalisti.",
-                    "Produzione e invio comunicati stampa / recalling / monitoraggio /
+                        "Produzione e invio comunicati stampa / recalling / monitoraggio /
                     rassegna stampa.",
-                    false
-                ) ?>
-            </div>
+                        false
+                    ) ?>
+                </div>
 
-            <div class="col-4">
-                <?= get_tile(
-                    "document-pen-sign-svgrepo-com",
-                    "Strategie di Comunicazione",
-                    "Un'ottima strategia di comunicazione è la base di qualsiasi attività che andremo a
+                <div class="col-12 col-sm-6 col-xl-4">
+                    <?= get_tile(
+                        "document-pen-sign-svgrepo-com",
+                        "Strategie di Comunicazione",
+                        "Un'ottima strategia di comunicazione è la base di qualsiasi attività che andremo a
                     sviluppare insieme.",
-                    "Analisi del mercato e dei competitors / analisi del target / definizione obiettivi /
+                        "Analisi del mercato e dei competitors / analisi del target / definizione obiettivi /
                     definizione strategia.",
-                    true
-                ) ?>
-            </div>
+                        true
+                    ) ?>
+                </div>
 
-            <div class="col-4">
-                <?= get_tile(
-                    "document-pen-sign-svgrepo-com",
-                    "Contenuti",
-                    "Ogni storia può essere raccontata ma è il modo con cui lo si fa a fare la differenza.",
-                    "Produzione di contenuti testuali e visivi per siti, blog, editoria, canali social.",
-                    false
-                ) ?>
-            </div>
+                <div class="col-12 col-sm-6 col-xl-4">
+                    <?= get_tile(
+                        "document-pen-sign-svgrepo-com",
+                        "Contenuti",
+                        "Ogni storia può essere raccontata ma è il modo con cui lo si fa a fare la differenza.",
+                        "Produzione di contenuti testuali e visivi per siti, blog, editoria, canali social.",
+                        false
+                    ) ?>
+                </div>
 
-            <div class="col-4">
-                <?= get_tile(
-                    "document-pen-sign-svgrepo-com",
-                    "Sociale Media Marketing",
-                    "I social network sono un canale di comunicazione fondamentale per poter sviluppare una
+                <div class="col-12 col-sm-6 col-xl-4">
+                    <?= get_tile(
+                        "document-pen-sign-svgrepo-com",
+                        "Sociale Media Marketing",
+                        "I social network sono un canale di comunicazione fondamentale per poter sviluppare una
                     strategia efficace. Sono un'importante opportunità di crescita e visibilità per aziende
                     e professionisti.",
-                    "Apertura profili / gestione / produzione contenuti testuali e grafici / campagne ADV.",
-                    true
-                ) ?>
-            </div>
+                        "Apertura profili / gestione / produzione contenuti testuali e grafici / campagne ADV.",
+                        true
+                    ) ?>
+                </div>
 
-            <div class="col-4">
-                <?= get_tile(
-                    "document-pen-sign-svgrepo-com",
-                    "Editoria Print Design",
-                    "Lookbooks, cataloghi, brochures, libri, flyers, riviste aziendali e 
+                <div class="col-12 col-sm-6 col-xl-4">
+                    <?= get_tile(
+                        "document-pen-sign-svgrepo-com",
+                        "Editoria Print Design",
+                        "Lookbooks, cataloghi, brochures, libri, flyers, riviste aziendali e 
                     presentazioni sono strumenti estremamente efficaci per rafforzare l'identità del brand.",
-                    "Studio grafico / Produzione di contenuti testuali / realizzazione.",
-                    false
-                ) ?>
-            </div>
+                        "Studio grafico / Produzione di contenuti testuali / realizzazione.",
+                        false
+                    ) ?>
+                </div>
 
-            <div class="col-4">
-                <?= get_tile(
-                    "document-pen-sign-svgrepo-com",
-                    "Eventi",
-                    "Eventi aziendali, lanci di prodotto, press day, manifestazioni di interesse comune.",
-                    "Ricerca location / allestimenti / ricerca catering /coordinamento / servizi tecnici.",
-                    true
-                ) ?>
-            </div>
+                <div class="col-12 col-sm-6 col-xl-4">
+                    <?= get_tile(
+                        "document-pen-sign-svgrepo-com",
+                        "Eventi",
+                        "Eventi aziendali, lanci di prodotto, press day, manifestazioni di interesse comune.",
+                        "Ricerca location / allestimenti / ricerca catering /coordinamento / servizi tecnici.",
+                        true
+                    ) ?>
+                </div>
 
-            <div class="col-4">
-                <?= get_tile(
-                    "document-pen-sign-svgrepo-com",
-                    "Brand Design",
-                    "Un'immagine visiva coordinata serve per definire e consolidare l'identità del brand.",
-                    "Progettazione Brand Image + Logo Design / studio della tipografia / studio del colore.",
-                    false
-                ) ?>
-            </div>
+                <div class="col-12 col-sm-6 col-xl-4">
+                    <?= get_tile(
+                        "document-pen-sign-svgrepo-com",
+                        "Brand Design",
+                        "Un'immagine visiva coordinata serve per definire e consolidare l'identità del brand.",
+                        "Progettazione Brand Image + Logo Design / studio della tipografia / studio del colore.",
+                        false
+                    ) ?>
+                </div>
 
-            <div class="col-4">
-                <?= get_tile(
-                    "document-pen-sign-svgrepo-com",
-                    "Realizzazione siti web",
-                    "Il web è la vetrina sul mondo aperta 365 giorni l'anno.
+                <div class="col-12 col-sm-6 col-xl-4">
+                    <?= get_tile(
+                        "document-pen-sign-svgrepo-com",
+                        "Realizzazione siti web",
+                        "Il web è la vetrina sul mondo aperta 365 giorni l'anno.
                     È quindi necessario non solo esserci ma esserci nel modo giusto.",
-                    "Progettazione e realizzazione di siti web su piattaforma  WordPress / servizio newsletter /
+                        "Progettazione e realizzazione di siti web su piattaforma  WordPress / servizio newsletter /
                     landing pages / e-commerce.",
-                    true
-                ) ?>
+                        true
+                    ) ?>
+                </div>
             </div>
         </div>
     </div>
