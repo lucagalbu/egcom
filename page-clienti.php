a2e9b5f46b553be17b75daab59fb860c50ea8c3c
lucagalbu <lucagalbu@gmail.com>
2023-09-16 20:54:25 +0200

    Clienti content is now entirely created in the admin board
    Before it displayed the client logos saved in the media.

--- page-clienti.php
+++ page-clienti.php
@@ -1,21 +1,4 @@
 <?php
-/**
- * Return the images of all posts of type logo.
- */
-function get_logos()
-{
-    $posts = get_posts(array(
-        'post_type' => 'logo',
-        'posts_per_page' => -1
-    ));
-
-    foreach ($posts as $post) { ?>
-        <div class="d-flex justify-content-center col-4 col-sm-2">
-            <?= get_the_post_thumbnail($post); ?>
-        </div>
-    <?php }
-}
-
 // Start the rendering of the page
 get_header(); 
 ?>
@@ -33,10 +16,8 @@ get_header();
         ); ?>
     </div>
 
-    <div class="container-xxl justify-content-center">
-        <div class="row gx-2 gy-4 my-5">
-            <?php get_logos(); ?>
-        </div>
+    <div class="outer-padding container-flex">
+        <?php the_content(); ?>
     </div>
 </div>
 
